99a62653f505e69b92dae1aa1c3a3729922340f9
Алексей Евгеньевич Баринов <BarinovAlE@mosreg.ru>
2018-12-18 16:25:42 +0300

    added ability to get password by login

--- src/main/kotlin/main/MainView.kt
+++ src/main/kotlin/main/MainView.kt
@@ -1,68 +1,41 @@
 package main
 
-import javafx.beans.property.SimpleListProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.scene.input.Clipboard
 import javafx.stage.Stage
 import tornadofx.*
 
 class MainView : View() {
     private val controller: MainController by inject()
-
-    private val textareaProperty = SimpleStringProperty("Yesterday all my troubles seemed so far away.\n" +
-            "Now it looks as though they're here to stay.\n" +
-            "Oh, I believe in yesterday.\n" +
-            "\n" +
-            "Suddenly, I'm not half the man I used to be.\n" +
-            "There's a shadow hanging over me.\n" +
-            "Oh, yesterday came suddenly.\n" +
-            "\n" +
-            "Why she had to go?\n" +
-            "I don't know, she wouldn't say.\n" +
-            "I said something wrong.\n" +
-            "Now I long for yesterday.\n" +
-            "\n" +
-            "Yesterday love was such an easy game to play.\n" +
-            "Now I need a place to hide away.\n" +
-            "Oh, I believe in yesterday.\n" +
-            "\n" +
-            "Why she had to go?\n" +
-            "I don't know, she wouldn't say.\n" +
-            "I said something wrong.\n" +
-            "Now I long for yesterday.\n" +
-            "\n" +
-            "Yesterday love was such an easy game to play.\n" +
-            "Now I need a place to hide away.\n" +
-            "Oh, I believe in yesterday")
-
-    private val listProperty = SimpleListProperty<String>()
-    private val count = SimpleStringProperty("")
-
+    private val input = SimpleStringProperty()
+    private val output = SimpleStringProperty()
 
     override val root = vbox {
-        textarea(textareaProperty) {
-            selectAll()
-        }
+        textfield(input)
 
-        button("Анализировать") {
+        button("Получить") {
             action {
-                controller.doAction(textareaProperty, listProperty, count)
+                controller.getPassword(input.get(), output)
             }
         }
 
-        listview<String>(listProperty)
-        hbox {
-            label("Количество уникальных слов: ")
-            label(count)
+        listview(controller.getLogins()) {
+            onUserSelect {
+                controller.getPassword(it, output)
+            }
         }
 
+        textfield(output)
     }
 }
 
 class TornadoApp : App(MainView::class) {
     override fun start(stage: Stage) {
-        stage.minWidth = 400.0
-        stage.minHeight = 400.0
+        stage.minWidth = 200.0
+        stage.minHeight = 100.0
+        stage.isAlwaysOnTop = true
         super.start(stage)
     }
 }
@@ -72,28 +45,27 @@ fun main(args: Array<String>) = run{
 }
 
 class MainController: Controller() {
-    fun doAction(input: SimpleStringProperty, output: SimpleListProperty<String>, count: SimpleStringProperty) {
-        var text = input.get().replace("\n", " ")
-        text = text.replace(".", " ")
-        text = text.replace(",", " ")
-        text = text.replace("?", " ")
-        text = text.replace("     ", " ")
-        text = text.replace("    ", " ")
-        text = text.replace("   ", " ")
-        text = text.replace("  ", " ")
-        val list = text.split(" ")
-        val hashMap: HashMap<String, Int> = HashMap()
-        for (item in list) {
-            val count = hashMap[item] ?: 0
-            hashMap[item] = count + 1
-        }
-        val map = hashMap.toList().sortedBy { (_, value) -> -1 * value}.toMap()
+    private val passwordMap = HashMap<String, String>()
+    private val loginList = ArrayList<String>()
 
-        val newList = ArrayList<String>()
-        for (item in map) {
-            newList.add(item.key + " " + item.value)
+    fun getPassword(input: String, output: SimpleStringProperty) {
+        if (passwordMap.isEmpty()) {
+            passwordMap.put("ftest", "ftest123")
+            passwordMap.put("support", "M5gisgKL")
+            passwordMap.put("ar2", "123321")
+            passwordMap.put("abarinov", "Abarinov")
         }
-        output.set(FXCollections.observableArrayList(newList))
-        count.set(newList.size.toString())
+        val currentPassword = passwordMap.getOrDefault(input, "Не найдено совпадений")
+        output.set(currentPassword)
+        val clipboard = Clipboard.getSystemClipboard()
+        clipboard.putString(currentPassword)
+    }
+
+    fun getLogins() : ObservableList<String> {
+        loginList.add("ftest")
+        loginList.add("support")
+        loginList.add("ar2")
+        loginList.add("abarinov")
+        return FXCollections.observableArrayList(loginList)
     }
 }
